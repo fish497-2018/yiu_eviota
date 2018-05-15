ff4bdf956c7f19eaf2ab429ed0a48bb6bfabdc65
Dara  Yiu <darayiu@D-10-19-230-16.dhcp4.washington.edu>
2018-05-15 14:00:05 -0700

    added and named data for basic analysis as 'morph'

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # yiu_eviota
+
+##data: 
+morph <- morphology_all_data
+
+file <- morphology_numerical_data3.csv
+contains cleaned data with eviota locality 
---
+++ scripts/morphology_analysis.R
@@ -0,0 +1,9 @@
+#read in morphology data file for basic comparison analysis
+morph <- read.csv("data/cleaned_data.csv")
+View(morph)
+
+
+#read in morphology data file
+file <- read.csv("data/morphology_numerical_data3.csv", header = TRUE, sep = ",")
+View(file)
+
