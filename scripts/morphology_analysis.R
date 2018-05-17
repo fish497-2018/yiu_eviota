315bc9af9c6de217cb6702416ba0799de79db60e
Dara  Yiu <darayiu@D-10-18-252-73.dhcp4.washington.edu>
2018-05-17 10:54:08 -0700

    try to subset 'traits'

--- scripts/morphology_analysis.R
+++ scripts/morphology_analysis.R
@@ -13,22 +13,29 @@ ggplot(data = morph) +
 Palau_PNG <- morph %>%
   filter(locality == "Papua New Guinea" | locality == "Palau")
 
-#4th pelvic branches histogram***
+#4th pelvic branches histogram*** # everything looks the same
 ggplot(data = Palau_PNG) +
   geom_histogram(binwidth = 0.5, mapping = aes(x = branches, fill = locality), show.legend = FALSE) +
   facet_wrap(~locality) +
   xlab("No. branches on 4th pelvic ray") +
   theme_classic(base_family = 'Arial', base_size = 10)
 
-#extract 
+#extract east & western haplotype data
 west <- filter(morph, haplotype == "west")
 east <- filter(morph, haplotype == "east")
 
-#compare characters from eastern and western haplotypes with a t tests
+#compare characters from eastern and western haplotypes with a t tests - suggests no difference between populations
 t.test(west$branches, east$branches)
+t.test(west$fifth_fourth_ratio, east$fifth_fourth_ratio)
+t.test(west$membrane_length, east$membrane_length)
 
-#read in morphology data file
+
+#read in morphology data file where stuff has been deleted
 file <- read.csv("data/morphology_numerical_data3.csv", header = TRUE, sep = ",")
 head(file)
+
+#define 'traits' as the numerical columns that the PCA will read
+traits <- data.matrix(file[,5:8]), rownames.force = NA)
+
 
 
